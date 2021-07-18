cc00350b6ec438aab3525bf25d2e3c8597024ab5
Mairon <mariusz.rogawski@gmail.com>
2021-07-19 00:09:55 +0200

    Working on Meals and Products

--- wildcalo/calccalo/admin.py
+++ wildcalo/calccalo/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Meals, Products
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'gender', 'age', 'weight', 'height', 'physical_activity', 'new_weight', 'time')
     list_filter = ('gender', 'physical_activity', 'time')
-# Register your models here.
+
+
+@admin.register(Meals)
+class MealsAdmin(admin.ModelAdmin):
+    list_display = ('person', 'name', 'products', 'kcal', 'prot', 'carb', 'fat')
+
+@admin.register(Products)
+class MealsProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'kcal', 'carb', 'prot', 'fat')
--- wildcalo/calccalo/models.py
+++ wildcalo/calccalo/models.py
@@ -55,3 +55,37 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profil użytkownika {}.'.format(self.user.username)
+
+
+class Meals(models.Model):
+    MEAL_CHOICES = [
+        ('breakfast','Breakfast'),
+        ('breakfast_2','II Breakfast'),
+        ('dinner', 'Dinner'),
+        ('lunch', 'Lunch'),
+        ('supper', 'Supper'),
+        ('snacks', 'Snacks')
+    ]
+
+    kcal = models.IntegerField(null=True, blank=True)
+    prot = models.IntegerField(null=True, blank=True)
+    carb = models.IntegerField(null=True, blank=True)
+    fat = models.IntegerField(null=True, blank=True)
+    person = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, choices=MEAL_CHOICES, default='breakfast')
+    products = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f'Posiłek {self.name} - {self.person}'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    kcal = models.IntegerField()
+    carb = models.IntegerField()
+    prot = models.IntegerField()
+    fat = models.IntegerField()
+
+
+
